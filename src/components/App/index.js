8d8dbe2e2415fdddb227b22d4f55cae1504f6733
grynnlxr <fabien@elixeer.fr>
2022-02-24 08:54:06 +0100

    styles ok

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,13 +2,14 @@ import Header from 'src/components/Header';
 import Form from 'src/components/Form';
 import Info from 'src/components/Info';
 
+import './style.scss';
 
 // == Composant
 function App() {
   return (
     <div className="app">
       <Header />
-      <main>
+      <main className='app-content'>
         <Form />
         <Info />
       </main>
